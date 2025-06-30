34b66347d19769f86a99a95aacd0075bd39187b1
Ascody <actaney@gmail.com>
2025-06-30 17:14:58 +0900

    structure

--- routes/home.py
+++ routes/home.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request,jsonify
+
+from services.data_provider import search_stock
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def home():
     return render_template('home.html')
+
+@home_bp.route('/search', methods=['POST'])
+def search():
+    q = request.form.get('q', "").strip().lower()
+    if not q:
+        return jsonify({'error': '검색어를 입력해주세요.'}), 400
+    stocks = search_stock(q)
+    return jsonify(stocks)
---
+++ services/data_provider.py
@@ -0,0 +1,5 @@
+def search_stock(keyword: str) -> list[dict]:
+    pass
+
+def get_stock_data(stock_code):
+    pass
